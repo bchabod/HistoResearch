b1cfcc97242b2b0c1ef27fd70924ded7e9d3736c
Benoit Chabod <be.chabod@gmail.com>
2015-11-24 08:33:14 +0100

    Final promise for URLs

--- routes/index.js
+++ routes/index.js
@@ -21,32 +21,39 @@ var getResult = function(keyword) {
   var offsets = [0,8,16,24,32,40,48,56];
   var links = [];
   var prevPromise = Promise.resolve();
-  offsets.forEach(function(offset) {  // loop through each title
-    prevPromise = prevPromise.then(function() {
-      var options = {
-        method: 'get',
-        url : 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=' + encodeURI(keyword) + '&start=' + offset,
-        headers: {
-          'Accept':'application/json',
+  var P = new Promise(function(resolve, reject) {
+    offsets.forEach(function(offset) {  // loop through each title
+      prevPromise = prevPromise.then(function() {
+        var options = {
+          method: 'get',
+          url : 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=' + encodeURI(keyword) + '&start=' + offset,
+          headers: {
+            'Accept':'application/json',
+          }
+        };
+        return rp(options);
+      }).then(function(body) {
+        body = JSON.parse(body);
+        for(var element in body["responseData"]["results"]){
+          var URL = body["responseData"]["results"][element]["url"];
+          links.push(URL);
         }
-      };
-      return rp(options);
-    }).then(function(body) {
-      body = JSON.parse(body);
-      for(var element in body["responseData"]["results"]){
-        var URL = body["responseData"]["results"][element]["url"];
-        links.push(URL);
-      }
-      console.log(links);
-    }).catch(function(error) {
-      console.log(error);
+        if(offset==56) {
+          resolve(links);
+        }
+      }).catch(function(error) {
+        reject(error);
+      });
     });
   });
+  return P;
 }
 
 router.get('/test', function(req, res, next) {
   var keyword = "Hello";
-  getResult(keyword);
+  getResult(keyword).then(function(allLinks) {
+    console.log("done : ", allLinks);
+  });
 });
 
 module.exports = router;
