a783c06e6b4ca8e2f75ab626157dfe2c5b0616a7
Benoit Chabod <be.chabod@gmail.com>
2015-11-24 08:26:00 +0100

    Chaining promises biatch

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var request = require('request');
+var rp = require('request-promise');
+var Promise = require('promise-js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var url = 'http://google.fr';
-
   var callback = function(error, response, body) {
     if (error || response.statusCode !== 200) {
       res.status(500).send();
@@ -13,9 +14,39 @@ router.get('/', function(req, res, next) {
       res.send(body);
     }
   };
-
   request(url, callback);
+});
 
+var getResult = function(keyword) {
+  var offsets = [0,8,16,24,32,40,48,56];
+  var links = [];
+  var prevPromise = Promise.resolve();
+  offsets.forEach(function(offset) {  // loop through each title
+    prevPromise = prevPromise.then(function() {
+      var options = {
+        method: 'get',
+        url : 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=' + encodeURI(keyword) + '&start=' + offset,
+        headers: {
+          'Accept':'application/json',
+        }
+      };
+      return rp(options);
+    }).then(function(body) {
+      body = JSON.parse(body);
+      for(var element in body["responseData"]["results"]){
+        var URL = body["responseData"]["results"][element]["url"];
+        links.push(URL);
+      }
+      console.log(links);
+    }).catch(function(error) {
+      console.log(error);
+    });
+  });
+}
+
+router.get('/test', function(req, res, next) {
+  var keyword = "Hello";
+  getResult(keyword);
 });
 
 module.exports = router;
